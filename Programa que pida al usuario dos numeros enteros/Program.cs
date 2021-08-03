29119a8c8dd0c423aa8a22050d0eb7ea7bb131cb
Nemo-Vill-Sant <SMIS032621@ugb.edu.sv>
2021-08-02 19:24:20 -0600

    Agregar archivos de proyecto.

---
+++ Programa que pida al usuario dos numeros enteros/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_que_pida_al_usuario_dos_numeros_enteros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite valor de a");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite vaalor de b");
+
+            int b = int.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+            
+            else if (b == 0)
+            {
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+
+            else
+            {
+                double x = a / b;
+            }
+
+            Console.WriteLine("Presione cualquir tecla para cerrar");
+
+            Console.ReadKey();
+        }
+    }
+}
